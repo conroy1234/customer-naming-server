bc825819dd702076b4d814417ae832cb26b17e0b
conroy.white <conroy.white@mastek.com>
2021-04-27 16:32:10 +0100

    initial commit

---
+++ src/main/java/com/customer/namine/server/CustomerNamingServerApplication.java
@@ -0,0 +1,13 @@
+package com.customer.namine.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerNamingServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/customer/namine/server/CustomerNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customer.namine.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
